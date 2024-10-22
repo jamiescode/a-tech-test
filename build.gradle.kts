31f791bf7373755a73252f1f59b450ceb73c9568
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-22 15:27:05 +0100

    Applied kotlin compose compiler, which is required for Kotlin 2.0 and above

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.kotlinter)
 }
