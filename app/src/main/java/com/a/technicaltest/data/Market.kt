42af80b8308162af167dc9f0e1ba76365d9a3ebb
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-23 23:12:13 +0100

    Extracted GetMarketsResponse.Market into Market (in own file)

--- app/src/main/java/com/a/technicaltest/data/Market.kt
+++ app/src/main/java/com/a/technicaltest/data/Market.kt
@@ -1,4 +1,19 @@
 package com.a.technicaltest.data
 
-class Market {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Market(
+    @SerializedName("Epic")
+    val epic: String,
+    @SerializedName("CompanyName")
+    val companyName: String,
+    @SerializedName("CurrentPrice")
+    val currentPrice: Double,
+    @SerializedName("CurrentChange")
+    val currentChange: Double,
+    @SerializedName("CurrentChangePct")
+    val currentChangePercentage: Double,
+) : Parcelable
--- app/src/main/java/com/a/technicaltest/list/MarketListEvent.kt
+++ app/src/main/java/com/a/technicaltest/list/MarketListEvent.kt
@@ -1,9 +1,9 @@
 package com.a.technicaltest.list
 
-import com.a.technicaltest.data.GetMarketsResponse
+import com.a.technicaltest.data.Market
 
 internal sealed class MarketListEvent {
     class ShowMarketDetail(
-        val market: GetMarketsResponse.Market,
+        val market: Market,
     ) : MarketListEvent()
 }
