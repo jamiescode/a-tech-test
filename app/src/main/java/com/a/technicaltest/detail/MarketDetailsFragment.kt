249afb6d5f3f820530d446b5918446ed9cee469c
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-24 18:43:41 +0100

    Now using view model

--- app/src/main/java/com/a/technicaltest/detail/MarketDetailsFragment.kt
+++ app/src/main/java/com/a/technicaltest/detail/MarketDetailsFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.a.technicaltest.databinding.FragmentMarketDetailBinding
 
 internal class MarketDetailsFragment : Fragment() {
+    private val viewModel by viewModels<MarketDetailViewModel>()
     private var binding: FragmentMarketDetailBinding? = null
+    private val args: MarketDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +28,16 @@ internal class MarketDetailsFragment : Fragment() {
     private fun initBinding() {
         val composeView = binding?.root
         composeView?.setContent {
-            MarketDetailScreen()
+            MarketDetailScreen(viewModel)
         }
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.setMarket(args.market)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/a/technicaltest/detail/MarketDetailsScreen.kt
+++ app/src/main/java/com/a/technicaltest/detail/MarketDetailsScreen.kt
@@ -3,10 +3,12 @@
 package com.a.technicaltest.detail
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,23 +17,62 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
+import androidx.lifecycle.asFlow
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.a.technicaltest.data.Market
 
 @Composable
-internal fun MarketDetailScreen() {
+internal fun MarketDetailScreen(viewModel: MarketDetailViewModel) {
     val navController = LocalView.current.findNavController()
     Scaffold(
         topBar = {
             AppBar(navController)
         },
         content = {
-            Box(modifier = Modifier.padding(it))
+            Box(modifier = Modifier.padding(it)) {
+                MarketDetailsScreenContent(viewModel)
+            }
         },
     )
+}
+
+@Composable
+private fun MarketDetailsScreenContent(viewModel: MarketDetailViewModel) {
+    val state =
+        viewModel.stateLiveData.asFlow().collectAsState(
+            initial = MarketDetailViewModel.State.Loading,
+        )
+
+    when (val value = state.value) {
+        is MarketDetailViewModel.State.Loading -> LoadingScreen()
+        is MarketDetailViewModel.State.Loaded -> LoadedScreen(value.market)
+    }
+}
+
+@Composable
+private fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun LoadedScreen(market: Market) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = "Company name: ${market.companyName}")
+    }
 }
 
 @Suppress("MagicNumber")
